2213c1ee1fafcb7d1a81d25ebd8ac7333dae8aa6
Elena Peris Mercé <45559386+elenapeme@users.noreply.github.com>
2021-03-26 22:21:15 +0100

    lista de canciones añadidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SongComponent } from './song/song.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SongComponent
+    SongComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Song } from '../interfaces/song';
+import { SONGS } from '../mock-songs';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+
+  songs = SONGS;
+
+  selectedSong?: Song;
+  onSelect(song: Song): void {
+    this.selectedSong = song;
+  }
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
