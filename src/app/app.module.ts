8d617ec0d12f6df665e07759b835e38f4072f4ea
elenaperis <elena@prestalo.com>
2021-05-21 18:47:40 +0200

    changed design landing page with material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatListModule, } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { SongFinishingPipe } from './pipes/song-finishing.pipe';
 
 @NgModule({
@@ -33,7 +34,8 @@ import { SongFinishingPipe } from './pipes/song-finishing.pipe';
     MatCardModule,
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
